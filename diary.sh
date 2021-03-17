f418f3ad76edc9d860fbee933de381085f6bf1ea
hezhizhen.hezz <hezhizhen.hezz@bytedance.com>
2021-03-17 19:36:09 +0800

    feat: add diary script to format diaries
    Change-Id: I633db80ec22e3e4e03d8868afa12f384da042dbd

---
+++ diary.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+cd ~/Dropbox/Diary
+today=$(date +"%Y-%m-%d")
+time=$(date +"%H:%M")
+file="$today.md"
+template="# $today
+
+* 星期：
+* 农历：
+* 天气：
+* 位置：
+* 睡眠：
+* 如厕：
+* 早餐：
+* 午餐：
+* 晚餐：
+* 在读：
+* 读完：
+"
+
+if [ ! -f "$file" ]; then
+    echo "$template" >> $file
+fi
+
+# append the current timestamp
+echo "## $time\n" >> $file
+
+code $file
