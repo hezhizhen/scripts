674dc159f5d76feeb695a4978043762c30926d2b
Zhizhen He <hezhizhen.yi@gmail.com>
2023-02-22 17:53:12 +0800

    feat: add savealbum.sh

---
+++ savealbum.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+link=$1                                                                                                    # $0 is the full path of this script, $1 to $n are arguments
+html=$(curl -L $link)                                                                                      # get HTML of this link
+title=$(echo $html | awk -vRS="</title>" '/<title>/{gsub(/.*<title>|\n+/,"");print;exit}')                 # extract title (copy from https://stackoverflow.com/questions/3833088/extract-title-of-a-html-file-using-grep)
+album=$(echo $title | awk -F " - Album by " -v OFS="" '{print $1}')                                        # extract album name
+singer=$(echo $title | awk -F " - Album by " -v OFS="" '{print $2}' | awk -F " | " -v OFS="" '{print $1}') # extract singer name
+echo "$singer,$album,$link" >>albums.csv                                                                   # save to local file
+sort -uo albums.csv albums.csv                                                                             # sort and remove duplicated lines
